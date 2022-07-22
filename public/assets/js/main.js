833de5a6c185098f79b06d3f60e852199a8033d5
Sama <thomasbenmezded@gmail.com>
2022-07-22 10:11:31 +0200

    update sakura and naruto mod

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -25,3 +25,57 @@ nightToggle.addEventListener('click', function (){
     }
     localStorage.setItem('nightActivated',"true");
 });
+
+
+
+
+//Je check si le mode light sakura est activé dans le local storage,
+//si oui je l'active avec le CSS
+if (localStorage.getItem('pinkActivated') === 'true') {
+    body.classList.add('pink-activated');
+}
+
+
+const lightToggle = document.querySelector('.js-kawaï-toggle');
+
+lightToggle.addEventListener('click', function (){
+    //Si le mode light sakura est activé je le désactive lorsque je click
+    if (body.classList.contains('pink-activated')) {
+        body.classList.remove('pink-activated');
+
+        //Je supp le mode darksasuke du local storage
+        localStorage.removeItem('pinkActivated');
+
+        //Si le mode dark sasuke n'est pas activé alors je l'active
+    } else {
+        body.classList.add('pink-activated');
+    }
+    localStorage.setItem('pinkActivated',"true");
+});
+
+
+
+
+
+
+if (localStorage.getItem('heroActivated') === 'true') {
+    body.classList.add('hero-activated');
+}
+
+
+const heroToggle = document.querySelector('.js-hero-toggle');
+
+heroToggle.addEventListener('click', function (){
+    //Si le mode naruto est activé je le désactive lorsque je click
+    if (body.classList.contains('hero-activated')) {
+        body.classList.remove('hero-activated');
+
+        //Je supp le mode naruto du local storage
+        localStorage.removeItem('heroActivated');
+
+        //Si le mode naruto n'est pas activé alors je l'active
+    } else {
+        body.classList.add('hero-activated');
+    }
+    localStorage.setItem('heroActivated',"true");
+});

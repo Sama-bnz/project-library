0c2f7ff2f83271c0e8078f00f55a2b779ea1de55
Sama <thomasbenmezded@gmail.com>
2022-07-22 09:50:30 +0200

    update dark sasuke mod all seen

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,14 +1,27 @@
 
 
-
-const nightToggle = document.querySelector('.js-night-toggle');
 const body = document.querySelector('.js-body');
 
-nightToggle.addEventListener('click', function (){
+//Je check si le mode dark sasuke est activé dans le local storage,
+//si oui je l'active avec le CSS
+if (localStorage.getItem('nightActivated') === 'true') {
+    body.classList.add('night-activated');
+}
 
+
+const nightToggle = document.querySelector('.js-night-toggle');
+
+nightToggle.addEventListener('click', function (){
+    //Si le mode dark sasuke est activé je le désactive lorsque je click
     if (body.classList.contains('night-activated')) {
         body.classList.remove('night-activated');
+
+        //Je supp le mode darksasuke du local storage
+        localStorage.removeItem('nightActivated');
+
+        //Si le mode dark sasuke n'est pas activé alors je l'active
     } else {
         body.classList.add('night-activated');
     }
+    localStorage.setItem('nightActivated',"true");
 });
